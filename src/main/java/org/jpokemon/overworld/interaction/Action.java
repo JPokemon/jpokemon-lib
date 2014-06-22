3f2217e56af9b8edb783578ae2ef727ece4d2264
Zach Taylor <zach.taylor673@gmail.com>
2014-06-22 12:49:48 -0500

    changed my mind
    The overworld interaction stuff belongs in JPokemon API

--- src/main/java/org/jpokemon/overworld/interaction/Action.java
+++
@@ -1,8 +0,0 @@
-package org.jpokemon.overworld.interaction;
-
-import org.jpokemon.api.JPokemonException;
-import org.jpokemon.api.PokemonTrainer;
-
-public interface Action {
-	public void execute(PokemonTrainer pokemonTrainer) throws JPokemonException;
-}
--- src/main/java/org/jpokemon/overworld/interaction/Requirement.java
+++
@@ -1,7 +0,0 @@
-package org.jpokemon.overworld.interaction;
-
-import org.jpokemon.api.PokemonTrainer;
-
-public interface Requirement {
-	public boolean isSatisfied(PokemonTrainer pokemonTrainer);
-}
