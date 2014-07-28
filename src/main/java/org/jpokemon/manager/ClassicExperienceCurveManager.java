ffaa7fe6e9d510c8d24bb387ac66fa372e2fff19
Zach Taylor <zach.taylor673@gmail.com>
2014-07-27 21:01:15 -0500

    moar cleanup

--- src/main/java/org/jpokemon/manager/ClassicExperienceCurveManager.java
+++ src/main/java/org/jpokemon/manager/ClassicExperienceCurveManager.java
@@ -1,14 +1,12 @@
 package org.jpokemon.manager;
 
 import org.jpokemon.api.ExperienceCurve;
-import org.jpokemon.api.JPokemonException;
 import org.jpokemon.experiencecurve.ErraticExperienceCurve;
 import org.jpokemon.experiencecurve.FastExperienceCurve;
 import org.jpokemon.experiencecurve.FluctuatingExperienceCurve;
 import org.jpokemon.experiencecurve.MediumFastExperienceCurve;
 import org.jpokemon.experiencecurve.MediumSlowExperienceCurve;
 import org.jpokemon.experiencecurve.SlowExperienceCurve;
-import org.jpokemon.manager.SimpleExperienceCurveManager;
 
 /**
  * Provides an example implementation of the {@link ExperienceCurve#manager},
@@ -25,27 +23,14 @@ import org.jpokemon.manager.SimpleExperienceCurveManager;
  * 
  * @since 0.1
  */
-public class ClassicExperienceCurveManager extends SimpleExperienceCurveManager {
+public class ClassicExperienceCurveManager extends SimpleManager<ExperienceCurve> {
 	public ClassicExperienceCurveManager() {
+		super(ExperienceCurve.class);
 		register(new ErraticExperienceCurve());
 		register(new FastExperienceCurve());
 		register(new FluctuatingExperienceCurve());
 		register(new MediumFastExperienceCurve());
 		register(new MediumSlowExperienceCurve());
 		register(new SlowExperienceCurve());
-	}
-
-	/**
-	 * Initializes a new ClassicExperienceCurveManager as the
-	 * {@link ExperienceCurve#manager}
-	 * 
-	 * @throws JPokemonException If the ExperienceCurve.manager is already defined
-	 */
-	public static void init() throws JPokemonException {
-		if (ExperienceCurve.manager != null) {
-			throw new JPokemonException("ExperienceCurve.manager is already defined");
-		}
-
-		ExperienceCurve.manager = new ClassicExperienceCurveManager();
 	}
 }
