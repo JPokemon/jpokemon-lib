f99a1cee4ee4986ed3fbc06d1ad101a1ba929d89
zach <zach.taylor673@gmail.com>
2014-09-24 13:07:01 -0500

    Fixed SimpleMovementSchemeManager

--- src/main/java/org/jpokemon/manager/SimpleMovementSchemeManager.java
+++ src/main/java/org/jpokemon/manager/SimpleMovementSchemeManager.java
@@ -1,16 +1,12 @@
 package org.jpokemon.manager;
 
-import org.jpokemon.api.Builder;
 import org.jpokemon.api.JPokemonException;
 import org.jpokemon.api.MovementScheme;
-import org.jpokemon.builder.SingletonBuilder;
 import org.jpokemon.movement.Hollow;
 import org.jpokemon.movement.Solid;
 
-public class SimpleMovementSchemeManager extends SimpleManager<Builder<MovementScheme>> {
-	@SuppressWarnings("unchecked")
+public class SimpleMovementSchemeManager extends SimpleBuildersManager<MovementScheme> {
 	public SimpleMovementSchemeManager() throws JPokemonException {
-		super((Class<Builder<MovementScheme>>) new SingletonBuilder<Object>(new Object()).getClass());
 		register(new Solid.Builder());
 		register(new Hollow.Builder());
 	}
